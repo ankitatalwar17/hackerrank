51e39f5add46a3d85c6fc147f90b87c5823190e8
Ankita <50921311+ankitatalwar17@users.noreply.github.com>
2019-06-02 19:14:23 +0530

    Create reverse.py

---
+++ python/arrays/reverse.py
@@ -0,0 +1,21 @@
+# Task 
+# Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+# Input Format
+# The first line contains an integer, N (the size of our array). 
+# The second line contains N space-separated integers describing array A's elements.
+
+# Constraints
+# 1 <= N <= 1000
+# 1 <= Ai <= 10000, where Ai is the ith integer in the array
+
+# Output Format
+# Print the elements of array A in reverse order as a single line of space-separated numbers.
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    new_ar =arr[::-1]
+    print (" ".join(str(ele) for ele in new_ar))
+    
